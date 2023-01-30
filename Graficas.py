f244e4411868aa00f9a8825707ff6a38a7e470b4
Diegofdiaz <diegodiaz4878@gmail.com>
2023-01-29 23:46:18 -0500

    update second versíon of graph code

--- Graficas.py
+++ Graficas.py
@@ -9,25 +9,37 @@ delta=0.01 #Representa el paso entre cada data a graficar
 h=np.arange(0,50000+delta,delta)
 b=(((1.8*10**(-3))*h)/(1+(10**(-3)*h)))
 x = Symbol('x')
-#-------------Primer dato---------------
+#-------------Primer dato----------------
 flujo1=1*(10**(-3)) #Wb
 b1=(flujo1/(0.004))
-h1=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b1))
-print(h1)
-#plt.plot(h1,b1, marker="o", color="red")
-#plt.show()
+print("b1=", b1)
+h1=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b1)) #Solve entrega la solucion como una lista, llamar h[0]
+print("h1=", h1)
+he1=b1/(4*np.pi*10**(-7))
+print("he1=", he1)
+i1=((h1[0]*0.7)+(h1[0]*0.25)+(he1*1*10**(-3)))/(800)
+print("I1=", i1)
 #-------------segundo dato---------------
 flujo2=5*(10**(-3)) #Wb
 b2=(flujo2/(0.004))
-h2=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b2))
-print(h2)
-#-------------tercer dato---------------
+print("b2=", b2)
+h2=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b2))#Solve entrega la solucion como una lista, llamar h[0]
+print("h2=", h2)
+he2=b2/(4*np.pi*10**(-7))
+print("he2=", he2)
+i2=((h2[0]*0.7)+(h2[0]*0.25)+(he2*1*10**(-3)))/(800)
+print("I2=", i2)
+#-------------tercer dato----------------
 flujo3=6.5*(10**(-3)) #Wb
 b3=(flujo3/(0.004))
-print(b3)
-h3=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b3))
-print(h3)
-#-------------------Graficas-------------------
+print("b3=", b3)
+h3=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b3)) #Solve entrega la solucion como una lista, llamar h[0]
+print("h3=", h3)
+he3=b3/(4*np.pi*10**(-7))
+print("he3=", he3)
+i3=((h3[0]*0.7)+(h3[0]*0.25)+(he3*1*10**(-3)))/(800)
+print("I3=", i3)
+#---------------Graficas-----------------
 fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
 ax1.plot(h,b)
 ax1.plot(h1,b1, marker="o", color="red")
--- Punto3.py
+++ Punto3.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sympy.solvers import solve
 from sympy import Symbol
+x=1
 flujo1=1*(10**(-3)) #Wb
 b1=(flujo1/(0.004))
 h1=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b1))
