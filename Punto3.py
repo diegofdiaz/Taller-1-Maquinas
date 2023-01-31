790825d26289ca17bd76e42619480cc53ffa5987
Diegofdiaz <diegodiaz4878@gmail.com>
2023-01-30 22:55:37 -0500

    add exercise 3

--- Punto3.py
+++ Punto3.py
@@ -2,8 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sympy.solvers import solve
 from sympy import Symbol
-x=1
-flujo1=1*(10**(-3)) #Wb
-b1=(flujo1/(0.004))
-h1=solve((((1.8*10**(-3))*x)/(1+(10**(-3)*x))-b1))
-print(h1)
+from scipy import constants as c
+ur=200 # permitividad relatividad, variable de entrada
+u=ur*c.mu_0
+x = Symbol('x')
+h1=solve((((2*10**(-3)))/(1+(10**(-3)*x))-u))
+b1=u*h1[0]
+he=b1/c.mu_0
+Fr=0.5*1000
+Fre=h1[0]*1 +he* 0.001
+error_a= Fre-Fr
+error_r= (abs(Fre-Fr)/Fr)*100
+while error_r > 3:
+    print(error_a)
+    if error_a < 0:
+        ur-=+10
+    else:
+        ur+=10
+    u=ur*c.mu_0
+    x = Symbol('x')
+    h1=solve((((2*10**(-3)))/(1+(10**(-3)*x))-u))
+    b1=u*h1[0]
+    he=b1/c.mu_0
+    Fr=0.5*1000
+    Fre=h1[0]*1 +he* 0.001
+    error_a= Fre-Fr
+    error_r=(abs(Fre-Fr)/Fr)*100
+print("El valor de la fuerza magnetomotriz encontrada fué :"+ str(Fre)+ ". Lo anterior con un error relativo de: ", error_r , "%")
+print("El valor de B encontrado fué: ", b1)
